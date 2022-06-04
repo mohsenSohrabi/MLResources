a385369afa673769398be048b59a840cb2d646c2
mohse <mohsen.sohrabi.cs@gmail.com>
2022-06-04 17:14:05 +0430

    Add project files.

---
+++ MLResources/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MLResources.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
